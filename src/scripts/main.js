a7b3da1a5c0a236c8aa703b772818df8948e780f
Dmytro <dmytrokovaliak@gmail.com>
2025-01-16 06:46:34 +0100

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,33 @@
 'use strict';
 
-// write your code here
+function cloneCeil(elements) {
+  const array = elements.map((el) => el.innerText);
+
+  return [...array.slice(0, -1), array[1], ...array.slice(-1)];
+}
+
+function insertToDom(element, create) {
+  const newElement = cloneCeil([...element.children]);
+
+  [...element.children].map((el) => el.remove());
+
+  newElement.map((item) => {
+    const newItem = document.createElement(create);
+
+    newItem.innerText = item;
+
+    element.insertAdjacentElement('beforeend', newItem);
+  });
+}
+
+const thead = document.querySelector('thead');
+const tbody = document.querySelector('tbody');
+const tfoot = document.querySelector('tfoot');
+const theadTr = thead.firstElementChild;
+const tfootTr = tfoot.firstElementChild;
+const tbodyAllTr = [...tbody.children];
+
+insertToDom(theadTr, 'th');
+insertToDom(tfootTr, 'th');
+
+tbodyAllTr.map((tbodyTr) => insertToDom(tbodyTr, 'td'));
